04f4d152ecf1476cc3565c4c5ad2e2c2d1276c57
felipedpgabriel <felipedpgabriel@gmail.com>
2023-12-04 16:12:25 -0300

    Parte 1 encerrada

--- src/main/java/io/sim/App.java
+++ src/main/java/io/sim/App.java
@@ -15,23 +15,25 @@ public class App
 {
    public static void main( String[] args ) throws InterruptedException
    {
-        // System.out.println("Inicia a simulacao");
-        // EnvSimulator ev = new EnvSimulator();
-        // ev.start(); // Inicia o simulador
-        // ev.join();
-        // System.exit(0); // Encerra o programa
+        //////// SISTEMA PRINCIPAL ////////
+        System.out.println("Inicia a simulacao");
+        EnvSimulator ev = new EnvSimulator();
+        ev.start(); // Inicia o simulador
+        ev.join();
+        System.exit(0); // Encerra o programa
 
-        try {
-            ExcelRepport.setFlowParam(26);
-            ExcelRepport.setStatistics(26);
-            double[][] recParam = ExcelRepport.getRecParam(26, 50);
+        //////// POS PROCESSAMENTO ////////
+    //     try {
+    //         ExcelRepport.setFlowParam(26);
+    //         ExcelRepport.setStatistics(26);
+    //         double[][] recParam = ExcelRepport.getRecParam(26, 50);
 
-            Rec rec = new Rec(recParam[0], recParam[1], recParam[2], recParam[3]);
-            rec.start();
-            rec.join();
-        } catch (EncryptedDocumentException | IOException e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
-        }
-    }
+    //         Rec rec = new Rec(recParam[0], recParam[1], recParam[2], recParam[3]);
+    //         rec.start();
+    //         rec.join();
+    //     } catch (EncryptedDocumentException | IOException e) {
+    //         // TODO Auto-generated catch block
+    //         e.printStackTrace();
+    //     }
+    // }
 }
